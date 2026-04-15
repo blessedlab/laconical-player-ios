2a16fe84f544f9446ab0917cba8ae75b81f5f3a0
Daniel Gidrewicz <daniel.hidrevich9@gmail.com>
2026-04-15 22:03:29 +0200

    Wire app entry and enable media permissions

--- Laconical Port/Laconical_PortApp.swift
+++ Laconical Port/Laconical_PortApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Laconical_PortApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaconicalPlayerRootView()
         }
     }
 }
